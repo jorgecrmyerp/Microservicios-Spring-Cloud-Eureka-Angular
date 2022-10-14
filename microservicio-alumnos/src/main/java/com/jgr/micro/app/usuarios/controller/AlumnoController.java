16fa650fa63bfdf380cf20fa13c91ee334d5b22f
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-14 12:56:08 +0200

    vi., 14 de oct. de 2022 12:55:222022-10-14 CLASE TEST DE ALUMNO

--- microservicio-alumnos/src/main/java/com/jgr/micro/app/usuarios/controller/AlumnoController.java
+++ microservicio-alumnos/src/main/java/com/jgr/micro/app/usuarios/controller/AlumnoController.java
@@ -27,72 +27,70 @@ public class AlumnoController {
 
 	@Autowired
 	private Tracer traceZipkin;
-	
+
 	@Autowired
 	private IAlumnoService iAlumnoService;
-	
-	
-	@GetMapping	
-	public ResponseEntity<?> listar(){
-		return ResponseEntity.ok().body(iAlumnoService.findAll());		
-		
+
+	@GetMapping
+	public ResponseEntity<?> listar() {
+		return ResponseEntity.ok().body(iAlumnoService.findAll());
+
 	}
-	
-	@GetMapping	("/{id}")
-	public ResponseEntity<?> buscaPorId(@PathVariable Long id){
-		
-		
+
+	@GetMapping("/{id}")
+	public ResponseEntity<?> buscaPorId(@PathVariable Long id) {
+
+		logger.info("busco alumno->" + id.toString());
 		Optional<Alumno> al = iAlumnoService.findById(id);
-		
+
 		if (!al.isPresent()) {
+			traceZipkin.currentSpan().tag("microservicioalumnos.AlumnoController.buscarPorId",
+					"alumno".concat(" no existe->".concat(id.toString())));
 			return ResponseEntity.notFound().build();
 		}
 		return ResponseEntity.ok().body(al);
 	}
-	
+
 	@PostMapping
-	public ResponseEntity<?> creaAlumno(@RequestBody Alumno al){
-		
-		Alumno alDb =iAlumnoService.save(al);
-		
+	public ResponseEntity<?> creaAlumno(@RequestBody Alumno al) {
+
+		Alumno alDb = iAlumnoService.save(al);
+
 		return ResponseEntity.status(HttpStatus.CREATED).body(alDb);
-		
+
 	}
-	
+
 	@PutMapping("/{id}")
-	public ResponseEntity<?> actualizaAlumno(@RequestBody Alumno al,@PathVariable Long id){
+	public ResponseEntity<?> actualizaAlumno(@RequestBody Alumno al, @PathVariable Long id) {
 
 		Optional<Alumno> o = iAlumnoService.findById(id);
-		
+
 		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
-		Alumno alDb =o.get();
-		
+		Alumno alDb = o.get();
+
 		alDb.setNombre(al.getNombre());
 		alDb.setApellidos(al.getApellidos());
 		alDb.setEmail(al.getEmail());
-		
-		
+
 		return ResponseEntity.ok().body(iAlumnoService.save(alDb));
-		
+
 	}
-	
+
 	@DeleteMapping("/{id}")
-	public ResponseEntity<?> borraAlumno(@PathVariable Long id){
+	public ResponseEntity<?> borraAlumno(@PathVariable Long id) {
 
 		Optional<Alumno> o = iAlumnoService.findById(id);
-		
+
 		if (!o.isPresent()) {
 			return ResponseEntity.notFound().build();
 		}
-		
+
 		iAlumnoService.deleteById(id);
-		
-		
+
 		return ResponseEntity.noContent().build();
-		
+
 	}
-	
 
 }
