0d711f026b3d65b83c46c1aedf0671ccb46154ab
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-23 13:13:52 +0200

    do., 23 de oct. de 2022 13:13:51 2022-10-22 ++query insert curso_alumnos

--- AB-microservicio-cursos/src/main/java/com/jgr/micro/app/cursos/MicroservicioCursosApplication.java
+++ AB-microservicio-cursos/src/main/java/com/jgr/micro/app/cursos/MicroservicioCursosApplication.java
@@ -9,11 +9,14 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 /**
  * The Class MicroservicioCursosApplication.
  * Prueba con Rest Repositories & Rest Repositories HAL Browser
- * como importa la clase alumno incluimos el package donde esta
+ * como importa la clase alumno incluimos el package donde esta,
+ * tambien hay que añadir donde esta el modelo de cursos para que no falle
  */
 @SpringBootApplication
 @EnableEurekaClient
-@EntityScan({"com.jgr.modelo.microservicio.datos.entity"})
+@EntityScan({"com.jgr.modelo.microservicio.datos.entity",
+"com.jgr.micro.app.cursos.models.data"}
+)
 public class MicroservicioCursosApplication {
 
 	/**
--- AB-microservicio-cursos/src/main/java/com/jgr/micro/app/cursos/controller/CursosController.java
+++ AB-microservicio-cursos/src/main/java/com/jgr/micro/app/cursos/controller/CursosController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 /**
  * The Class CursosController.
+ * LA RELACION CON LA CLASE ESTA EN ICursoRepository, 
  */
 @RestController
 public class CursosController {
