f05cd456a9f0c315afa784659980abaf352c545e
jorgecrmyerp <jorgecrmyerp@gmail.com>
2022-10-27 15:08:57 +0200

    ju., 27 de oct. de 2022 15:04:572022-10-27 MICROSERVICIO RESPUESTAS GATEWAY CONFIG

--- gitprune.sh
+++ gitprune.sh
@@ -15,7 +15,7 @@ PushLog="/dev/null"
 # Mensaje
 message=$@
 if [ -z "$message" ]; then
-message="$(date)2022-10-27 MICROSERVICIO RESPUESTAS SIN CONFIGURAR"
+message="$(date)2022-10-27 MICROSERVICIO RESPUESTAS GATEWAY CONFIG"
 fi
 
 start=`date +%s`
